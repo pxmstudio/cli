207bd69c209cfdb78ed7ac815e1a50500d9f2e98
Mihai Ungureanu <mihai@pxm.digital>
2025-06-14 21:12:19 +0300

    feat: improve Webflow website next steps

---
+++ wf-proj/vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+
+export default defineConfig({
+    base: "./",
+    plugins: [cloudflare()],
+    server: {
+        host: "localhost",
+        cors: {
+            origin: "*",
+        },
+        port: 5173,
+        hmr: {
+            host: "localhost",
+            protocol: "ws",
+        },
+    },
+    build: {
+        minify: true,
+        manifest: true,
+        outDir: "dist",
+        assetsDir: "assets",
+        rollupOptions: {
+            input: {
+                main: "./src/main.ts",
+            },
+            output: {
+                format: "es",
+                entryFileNames: "main.js",
+                chunkFileNames: "assets/[name].[hash].js",
+                assetFileNames: "assets/[name].[hash][extname]",
+                compact: true,
+                globals: {
+                    jquery: "$",
+                },
+            },
+            external: ["jquery"],
+        },
+        modulePreload: {
+            polyfill: true,
+        }
+    },
+});
